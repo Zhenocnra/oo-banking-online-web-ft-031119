0e4ab626aaedf5c36bcb6ac4eab7b4803d6cc293
Zoncharne Lee <zoncharne@gmail.com>
2019-03-27 10:54:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
 
+  def initialize(sender, receiver, transfer_amt)
+    @sender = sender
+    @receiver = receiver
+    @transfer_amt = transfer_amt
+    @status = "pending"
+  end
+
 end
