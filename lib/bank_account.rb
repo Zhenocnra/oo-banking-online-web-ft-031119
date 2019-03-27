6c12fc6263b07a2db7eaa87eb7d813a3e5cb57fd
Zoncharne Lee <zoncharne@gmail.com>
2019-03-27 10:48:18 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,9 +13,11 @@ class BankAccount
   end
   
   def display_balance
+    "Your balance is $#{self.balance}."
   end
   
   def valid
+    balance > 0 && status == "open"
   end
   
   def close_account
