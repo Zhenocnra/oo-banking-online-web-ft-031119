5f27126cbe9e393d80b6752fb999e5f2f5181ada
Zoncharne Lee <zoncharne@gmail.com>
2019-03-27 10:58:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
-  attr_accessor :sender, :receiver, :transfer_amt, :status
-
+  attr_accessor :sender, :receiver, :status
+  attr_reader :transfer_amt
   def initialize(sender, receiver, transfer_amt)
     @sender = sender
     @receiver = receiver
     @transfer_amt = transfer_amt
     @status = "pending"
   end
+
 
 end
