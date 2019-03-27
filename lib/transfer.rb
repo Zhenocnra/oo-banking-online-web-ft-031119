e9f83e9582259edea53362ad9a36a686aad4cc7c
Zoncharne Lee <zoncharne@gmail.com>
2019-03-27 10:58:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,7 @@
 class Transfer
   attr_accessor :sender, :receiver, :status
   attr_reader :transfer_amt
+  
   def initialize(sender, receiver, transfer_amt)
     @sender = sender
     @receiver = receiver
